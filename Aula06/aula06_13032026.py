b301e9635fffb46b113b946f5923727daf2e0677
KlemDoug <klemdoug@gmail.com>
2026-03-13 21:43:55 -0300

    adiciona aulas 5 e 6

---
+++ Aula06/aula06_13032026.py
@@ -0,0 +1,18 @@
+# LISTAS:
+
+nomes = ['Matheus','Alice','Caio','Larissa','Miguel','Rafael']
+
+nome1 = nomes[0]
+
+print(nomes[-1])
+print(nomes[-2])
+print(nomes[-3])
+print(nomes[-4])
+print(nome1)
+
+primeira_parte=nomes[0:3]
+print(primeira_parte)
+segunda_parte=nomes[3:6]
+print(segunda_parte)
+
+print(len(nomes))
